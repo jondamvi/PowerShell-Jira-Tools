4817b3b4859b190801ac8fb0f73d4a1b861bd805
JonDam7 <jon.damvi@gmail.com>
2026-09-09 08:58:23 +0000

    Add files via upload

--- diag_page.groovy
+++ diag_page.groovy
@@ -2,15 +2,13 @@ import com.onresolve.scriptrunner.db.DatabaseUtil
 import groovy.sql.Sql
 DatabaseUtil.withSql('ConfluenceDB') { Sql sql ->
     def out = new StringBuilder()
-    String needle = 'ac:name="last-modified"'   // <-- your real target macro name
-    sql.eachRow('''SELECT c.contentid, c.content_status,
-                          position(:needle in b.body) AS at,
-                          length(b.body) AS len
+    sql.eachRow('''SELECT c.content_status, c.contenttype,
+                     (length(b.body) - length(replace(b.body, 'ac:name="last-modified"', ''))) / length('ac:name="last-modified"') AS exact_hits,
+                     (length(b.body) - length(replace(b.body, 'last-modified', ''))) / length('last-modified') AS loose_hits
                    FROM content c JOIN bodycontent b ON b.contentid = c.contentid
-                   WHERE c.contentid = :pid''', [pid: 123456L, needle: needle]) { r ->
-        out.append('status=').append(r['content_status'])
-           .append(' bodylen=').append(r['len'])
-           .append(' needle_at=').append(r['at']).append('\n')
+                   WHERE c.contentid = :pid''', [pid: 123456L]) { r ->
+        out.append('status=').append(r['content_status']).append(' type=').append(r['contenttype'])
+           .append(' exact=').append(r['exact_hits']).append(' loose=').append(r['loose_hits']).append('\n')
     }
-    return "<pre>" + out.toString() + "</pre>"
+    return "<pre>"+out.toString()+"</pre>"
 }
