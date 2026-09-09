cbf96a767ae7a157f36fd236a60851c57ff7e603
JonDam7 <jon.damvi@gmail.com>
2026-09-09 08:09:27 +0000

    Add files via upload

--- diag_page.groovy
+++ diag_page.groovy
@@ -2,14 +2,15 @@ import com.onresolve.scriptrunner.db.DatabaseUtil
 import groovy.sql.Sql
 DatabaseUtil.withSql('ConfluenceDB') { Sql sql ->
     def out = new StringBuilder()
-    sql.eachRow('''SELECT c.contentid, c.content_status, c.contenttype,
-                          position('ac:name' in b.body) AS has_acname,
-                          substring(b.body from position('ac:name' in b.body) for 60) AS sample
+    String needle = 'ac:name="last-modified"'   // <-- your real target macro name
+    sql.eachRow('''SELECT c.contentid, c.content_status,
+                          position(:needle in b.body) AS at,
+                          length(b.body) AS len
                    FROM content c JOIN bodycontent b ON b.contentid = c.contentid
-                   WHERE c.contentid = :pid''', [pid: 123456L]) { r ->
-        out.append(r['content_status']).append(' / ').append(r['contenttype'])
-           .append(' / acname@').append(r['has_acname'])
-           .append(' / ').append(r['sample']).append('\n')
+                   WHERE c.contentid = :pid''', [pid: 123456L, needle: needle]) { r ->
+        out.append('status=').append(r['content_status'])
+           .append(' bodylen=').append(r['len'])
+           .append(' needle_at=').append(r['at']).append('\n')
     }
-    return "<pre>" + out.toString().replace('<','&lt;') + "</pre>"
+    return "<pre>" + out.toString() + "</pre>"
 }
